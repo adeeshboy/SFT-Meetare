9b450c01d9658505d23717243d27c02b177cad97
adeeshboy <adeeshboy0@gmail.com>
2026-07-08 17:24:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,56 @@
 const questions = [
 {
-    question: "CPU යන්නෙහි සම්පූර්ණ නම කුමක්ද?",
-    options: [
-        "Central Processing Unit",
-        "Computer Processing Unit",
-        "Central Program Unit",
-        "Computer Program Utility"
-    ],
-    answer: 0
+question:"CPU යන්නෙහි සම්පූර්ණ නම කුමක්ද?",
+options:[
+"Central Processing Unit",
+"Computer Processing Unit",
+"Central Program Unit",
+"Computer Program Utility"
+],
+answer:0
 }
 ];
 
-const quiz = document.getElementById("quiz");
+const quiz=document.getElementById("quiz");
 
-questions.forEach((q, index) => {
+questions.forEach((q,index)=>{
 
-    let html = `
-    <div class="question">
-        <h3>${index + 1}. ${q.question}</h3>
-    `;
+let html=`<div class="question">
+<h3>${index+1}. ${q.question}</h3>`;
 
-    q.options.forEach((option, i) => {
-        html += `
-        <label>
-            <input type="radio" name="q${index}" value="${i}">
-            ${option}
-        </label><br><br>
-        `;
-    });
+q.options.forEach((option,i)=>{
+html+=`
+<label>
+<input type="radio" name="q${index}" value="${i}">
+${option}
+</label>`;
+});
 
-    html += `</div>`;
+html+=`</div>`;
 
-    quiz.innerHTML += html;
+quiz.innerHTML+=html;
+
 });
 
 function submitQuiz(){
 
-    let correct = 0;
+let correct=0;
 
-    questions.forEach((q,index)=>{
+questions.forEach((q,index)=>{
 
-        const selected = document.querySelector(`input[name="q${index}"]:checked`);
+const selected=document.querySelector(`input[name="q${index}"]:checked`);
 
-        if(selected && Number(selected.value)===q.answer){
-            correct++;
-        }
+if(selected && Number(selected.value)===q.answer){
+correct++;
+}
 
-    });
+});
 
-    document.getElementById("result").innerHTML =
-    `✅ Correct : ${correct}<br>
-     ❌ Wrong : ${questions.length-correct}<br>
-     📊 Score : ${correct}/${questions.length}`;
+document.getElementById("result").innerHTML=
+`
+✅ Correct : ${correct}<br>
+❌ Wrong : ${questions.length-correct}<br>
+📊 Score : ${correct}/${questions.length}
+`;
+
 }
