2af2153d8fbc4e939424b04d9a1a6edf2ed168b7
adeeshboy <adeeshboy0@gmail.com>
2026-07-08 17:38:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-const questions = [
-{
-question:"CPU යන්නෙහි සම්පූර්ණ නම කුමක්ද?",
-options:[
-"Central Processing Unit",
-"Computer Processing Unit",
-"Central Program Unit",
-"Computer Program Utility"
-],
-answer:0
+let currentQuestion = 0;
+let userAnswers = [];
+
+function loadQuestion() {
+
+    document.getElementById("progress").innerHTML =
+    "Question " + (currentQuestion + 1) + " / " + questions.length;
+
+    let q = questions[currentQuestion];
+
+    let html = `
+    <div class="question">
+        <h2>${q.question}</h2>
+    `;
+
+    q.options.forEach((option,index)=>{
+
+        html += `
+        <label>
+        <input type="radio"
+        name="answer"
+        value="${index}">
+        ${option}
+        </label>
+        `;
+
+    });
+
+    html += "</div>";
+
+    document.getElementById("quiz").innerHTML = html;
+
 }
-];
 
-const quiz=document.getElementById("quiz");
-
-questions.forEach((q,index)=>{
-
-let html=`<div class="question">
-<h3>${index+1}. ${q.question}</h3>`;
-
-q.options.forEach((option,i)=>{
-html+=`
-<label>
-<input type="radio" name="q${index}" value="${i}">
-${option}
-</label>`;
-});
-
-html+=`</div>`;
-
-quiz.innerHTML+=html;
-
-});
-
-function submitQuiz(){
-
-let correct=0;
+loadQuestion();let correct=0;
 
 questions.forEach((q,index)=>{
 
